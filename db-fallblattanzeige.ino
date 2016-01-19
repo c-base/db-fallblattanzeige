79cc6b7cd7c43d9ac345916f8ae403ca18cb7bee
coon <coon@c-base.org>
2016-01-19 18:38:06 +0100

    modified code for prototype pcb

--- db-fallblattanzeige.ino
+++ db-fallblattanzeige.ino
@@ -1,19 +1,27 @@
 // DB-Fallblattanzeige 
-// 05.01.2016
+// 15.01.2016
 //
 // Authors: uk, coon
 
-const int PIN_MOTOR = 2;
-const int PIN_SENSORS = 3;
-const int ORIGIN_OFFSET = 6;
+// Pins
+const int PIN_SENSORS_IN = 5;  // PD5
+const int PIN_SR_LATCH   = 6;  // PD6
+const int PIN_SR_CP      = 7;  // PD7
+const int PIN_SR_DATA    = 8;  // PB0
+const int PIN_MOTOR      = 9;  // PB1
+const int PIN_IR_ROTARY  = A2; // PC2
+const int PIN_IR_ORIGIN  = A3; // PC3
+
+// constants
+const int ORIGIN_OFFSET = 5;
 
 void waitForSwitches(int switches) {
-  int lastValue = digitalRead(PIN_SENSORS);
+  int lastValue = digitalRead(PIN_SENSORS_IN);
   int currentValue;
   int numSwitches = 0;
   
   while(numSwitches < switches) {
-    currentValue = digitalRead(PIN_SENSORS);
+    currentValue = digitalRead(PIN_SENSORS_IN);
     delayMicroseconds(50);
 
     if(currentValue != lastValue) {
@@ -24,40 +32,41 @@ void waitForSwitches(int switches) {
 }
 
 void flipCards(int numCards) {
+  digitalWrite(PIN_IR_ROTARY, HIGH);
   digitalWrite(PIN_MOTOR, LOW);
   
   for(int i = 0; i < numCards; i++)
     waitForSwitches(2);
     
   digitalWrite(PIN_MOTOR, HIGH);
+  digitalWrite(PIN_IR_ROTARY, LOW);
 }
 
 void spinToOrigin() {
+  digitalWrite(PIN_IR_ORIGIN, HIGH);
   digitalWrite(PIN_MOTOR, LOW);
 
-  // skip first high value
-  while(digitalRead(PIN_SENSORS));
-
-  int startValue = digitalRead(PIN_SENSORS);
-  int numEqual = 0;
+  while(digitalRead(PIN_SENSORS_IN));  // skip first high value
+  while(!digitalRead(PIN_SENSORS_IN)); // wait for home  
+  while(digitalRead(PIN_SENSORS_IN));  // wait for end of home
+  flipCards(ORIGIN_OFFSET);            // flip some additional cards, if needed
   
-  while(numEqual < 1000) {   
-    if(HIGH == digitalRead(PIN_SENSORS))
-      numEqual++;
-    else
-      numEqual = 0;
-
-    delayMicroseconds(100);
-  }
-  while(digitalRead(PIN_SENSORS));
-
-  flipCards(ORIGIN_OFFSET);
   digitalWrite(PIN_MOTOR, HIGH);
+  digitalWrite(PIN_IR_ORIGIN, LOW);
 }
 
 void setup() {
-  pinMode(PIN_MOTOR, OUTPUT);
-  pinMode(PIN_SENSORS, INPUT);
+  pinMode(PIN_SENSORS_IN, INPUT);
+  pinMode(PIN_SR_LATCH,   OUTPUT);
+  pinMode(PIN_SR_CP,      OUTPUT);
+  pinMode(PIN_SR_DATA,    OUTPUT);
+  pinMode(PIN_MOTOR,      OUTPUT);
+  pinMode(PIN_IR_ORIGIN,  OUTPUT);
+  pinMode(PIN_IR_ROTARY,  OUTPUT);
+  
+  digitalWrite(PIN_IR_ORIGIN, LOW);
+  digitalWrite(PIN_IR_ROTARY, LOW);
+  
   Serial.begin(9600);
 
   spinToOrigin();
@@ -72,4 +81,5 @@ void loop() {
       spinToOrigin();
   }
 }
+
 
